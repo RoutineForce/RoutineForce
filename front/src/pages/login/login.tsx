f8d1771ff49d634072b96623e71f0e5f76d7ff4e
jaewpark <ohdiar@gmail.com>
2021-11-24 18:24:08 +0900

    add front_LogIn

--- front/src/pages/login/login.tsx
+++ front/src/pages/login/login.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import PageMover from '../../utils/PageMover';
+import GoogleLogin from 'react-google-login';
 
 /**
  * Todo : jaewpark 이 로그인 페이지 만들어줄거임! ㅎㅎ
@@ -12,5 +13,51 @@ export default function Login(): JSX.Element {
     };
     window.open(process.env.REACT_APP_42_LOGIN_URL);
   };
-  return <button onClick={login42ButtonClick}>42 Login</button>;
+  // KAKAO_LOGIN_URL 체크
+  const loginKakaoButtonClick = () => {
+    (window as any).setCookieAndBack = (jwtToken: string) => {
+      PageMover.goBack();
+    };
+    window.open(process.env.REACT_APP_KAKAO_LOGIN_URL);
+  };
+
+  // GOOGLE_LOGIN_URL 체크
+  // const googleClientId = 'process.env.REACT_APP_CLIENT_ID || ""';
+  // const [userObj, setUserObj] = React.useState({
+  //   email: '',
+  //   name: '',
+  // });
+  // //로그인 성공시 res처리
+  // const onLoginSuccess = (res: any) => {
+  //   setUserObj({
+  //     ...userObj,
+  //     email: res.profileObj.email,
+  //     name: res.profileObj.name,
+  //   });
+  // };
+
+  const loginGoogleButtonClick = () => {
+    (window as any).setCookieAndBack = (jwtToken: string) => {
+      PageMover.goBack();
+    };
+    window.open(process.env.REACT_APP_CLIENT_ID);
+  };
+  return (
+    <div>
+      <button onClick={login42ButtonClick}>42 Login</button>
+      <img
+        src={'./kakao_logo.png'}
+        onClick={loginKakaoButtonClick}
+        id="kakao-login-btn"
+        width="250px"
+      />
+      {/* <GoogleLogin
+        clientId={googleClientId}
+        buttonText="Google"
+        onSuccess={result => onLoginSuccess(result)}
+        onFailure={result => console.log(result)}
+      /> */}
+      <button onClick={loginGoogleButtonClick}>Google Login</button>
+    </div>
+  );
 }
