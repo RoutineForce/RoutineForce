076bc1f96bd1abf787b476b1811820d603d3ba56
sohan <sohan@student.42seoul.kr>
2021-11-29 12:20:42 +0000

    [feat] get all routines with status='pending'

--- back/routineforce/views.py
+++ back/routineforce/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .serializers import RoutineSerializer 
 from .models import Routine
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -14,3 +15,9 @@ def test(request):
 class RoutineViewSet(viewsets.ModelViewSet):
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
+
+    @action(detail=False)
+    def pending(self, request):
+        qs = Routine.objects.filter(status='pending')
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
