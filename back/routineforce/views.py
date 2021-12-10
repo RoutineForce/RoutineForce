f851795f14f055e6746c2fcd911e530e7731b310
sohan <sohan@student.42seoul.kr>
2021-12-10 11:01:17 +0000

    moji?

--- back/routineforce/views.py
+++ back/routineforce/views.py
@@ -104,19 +104,26 @@ class UserViewSet(viewsets.ModelViewSet):
 routine_url = "http://ec2-13-124-86-205.ap-northeast-2.compute.amazonaws.com:8000"
 REST_API_KEY = "10597e3006de4b770f6463b53a4324d2"
 redirect_uri = "http://localhost:3000/loginreturn/kakao"
+APP_ADMIN_KEY = "0c125d342704bcb22e4b4df1119ecafc"
 class LoginAPI(APIView):
 
-    #def get(self, request):
-    #    return Respones()
+    def get(self, request):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'KakaoAK {APP_ADMIN_KEY}',
+        }
+
+        data = {
+            'target_id_type': 'user_id',
+            'target_id': '2027486874'
+        }
+        response = requests.post('https://kapi.kakao.com/v1/user/unlink', headers=headers, data=data)
+        return Response("logout")
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             #do login
             code=serializer.data.get('code')
-            #print(code)
-            #print(type(code))
-            #print(serializer.data)
-            #print(type(serializer.data))
             service=serializer.data.get('service')
             headers = {
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -128,14 +135,9 @@ class LoginAPI(APIView):
                 'redirect_uri': f'{redirect_uri}',
                 'code': f'{code}',
             }
-            #print(data)
             token_response = requests.post('https://kauth.kakao.com/oauth/token', headers=headers, data=data)
             response_json = token_response.json()
-            #print(response_json)
-            #print(type(response_json))
-            #print(response_json["access_token"])
             access_token = response_json["access_token"]
-            #access_token = "RDQg-SS0nqn_bWHvoqjccAvaUU_pY_coFS-4PwopyNgAAAF9ovDXwQ"
             headers = {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization': f'Bearer {access_token}',
@@ -146,9 +148,37 @@ class LoginAPI(APIView):
             }
             
             kakao_response = requests.post('https://kapi.kakao.com/v2/user/me', headers=headers)
-            routineforce_user_response = requests.get('http://ec2-13-124-86-205.ap-northeast-2.compute.amazonaws.com:8000/user')
-            print(kakao_response.text)
-            print(routineforce_user_response)
+            kakao_response_json = kakao_response.json()
+            kakao_id = kakao_response_json["id"]
+            kakao_account = kakao_response_json["kakao_account"]
+            kakao_profile = kakao_account["profile"]
+            kakao_name = kakao_profile["nickname"]
+            kakao_image = kakao_profile["profile_image_url"]
+            kakao_email = kakao_account["email"]
+            user_params = { 
+                    "id" : f"{kakao_id}", 
+                    "login" : f"{service}"
+            }
+            routineforce_user_response = requests.get('http://ec2-13-124-86-205.ap-northeast-2.compute.amazonaws.com:8000/user', params = user_params)
+            #if routineforce_user_response.text == "[]":
+            if User.objects.filter(id=kakao_id, login=service).exists():
+                print("user exists")
+            else :
+                User(
+                        id = kakao_id,
+                        login = service,
+                        name = kakao_name,
+                        email = kakao_email,
+                        image_path = kakao_image
+                ).save()
+                #data = {
+                #        "id" : f"{kakao_id}",
+                #        "login": f"{service}",
+                #        "name" :f"{kakao_name}",
+                #        "email": f"{kakao_email}",
+                #        "image_path" :f"{kakao_image}"
+                #}
+                #r = requests.post('http://ec2-13-124-86-205.ap-northeast-2.compute.amazonaws.com:8000/user/', data=data)
             return Response('Login Successed!' ,status=status.HTTP_200_OK)
         else :
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
