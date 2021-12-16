710311a2fde9cacf83a9fe890cfc04e4389782e9
jaewpark <ohdiar@gmail.com>
2021-12-16 16:55:05 +0900

    대댓글 진행 중, 백엔드와 합치기 위한 준비

---
+++ front/src/components/viewRoutine/RoutineReplyComment.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import RoutineComment from './RoutineComment';
+
+export default function RoutineReplyComment(
+  post_id: number,
+  Comments: any,
+  setComments: any,
+  comment: any,
+) {
+  const [childCommentNumber, setChildCommentNumber] = useState(0);
+  const [openReply, setOpenReply] = useState(false);
+
+  const onClickViewMore = () => {
+    setOpenReply(!openReply);
+  };
+
+  useEffect(() => {
+    let commentNumber = 0;
+    Comments.map((el: any) => {
+      if (el.responseTo === comment._id) {
+        commentNumber++;
+      }
+    });
+    setChildCommentNumber(commentNumber);
+  }, [Comments]);
+
+  const RenderReply = (parentId: number) =>
+    Comments.map((comment: string, index: number) => {
+      <>
+      {comment.responseTo === parentId && (
+        <div style={{width: "80%" , marginLeft: "40px"}}>
+          <RoutineComment
+            key={index}
+            //https://darrengwon.tistory.com/828?category=858368
+          />
+        </div>
+      )}
+      </>
+    })
+  return (
+    <>
+    </>
+  );
+}
