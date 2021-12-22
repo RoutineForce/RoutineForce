d4f20b9bd13b9a8156df74335c6836cd1931123c
jaewpark <ohdiar@gmail.com>
2021-12-22 12:34:11 +0900

    사진 인증 시간에 맞춰 인증하기

--- front/src/components/viewRoutine/pictureUpload.tsx
+++ front/src/components/viewRoutine/pictureUpload.tsx
@@ -1,8 +1,13 @@
 import React, {useState, useRef} from 'react';
+import {Modal} from 'react-bootstrap';
+import {Icon} from 'semantic-ui-react';
 
 // 사진 저장에 필요한 무언가 확인, 날짜와 닉네임
 export default function PictureUpload(): JSX.Element {
   const [imgBlob, setImgBlob] = useState('');
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const filePickerRef = useRef<HTMLInputElement>(null);
 
@@ -22,15 +27,65 @@ export default function PictureUpload(): JSX.Element {
     reader.readAsDataURL(file);
   };
 
+  function Timecheck() {
+    const nowTime = new Date();
+    const nowHourMinute = {
+      hour: nowTime.getHours(),
+      minute: nowTime.getMinutes(),
+    };
+    const TimeSort = [
+      nowHourMinute,
+      {hour: 8, minute: 0},
+      {hour: 9, minute: 0},
+    ];
+    TimeSort.sort(function (a, b) {
+      if (a.minute >= b.minute) {
+        return 1;
+      }
+      return -1;
+    });
+    TimeSort.sort(function (a, b) {
+      if (a.hour >= b.hour) return 1;
+      return -1;
+    });
+    console.log(TimeSort);
+    if (TimeSort[1] == nowHourMinute) return imgClick();
+    return handleShow();
+  }
+  const checkBox = [
+    {hour: 8, minute: 0},
+    {hour: 9, minute: 0},
+  ];
+
   return (
     <div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="sm"
+        aria-labelledby="contained-modal-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>인증 시간</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Icon name="clock outline" /> 6:00 ~ 6:30
+          <p>시간에 맞춰서 인증해주세요</p>
+        </Modal.Body>
+      </Modal>
       <div
         className="CertificationForm"
-        onClick={imgClick}
+        onClick={Timecheck}
         style={{
           backgroundImage: `url(${imgBlob})`,
+          cursor: 'pointer',
         }}
-      ></div>
+      >
+        <span>
+          {checkBox[1].hour.toString() + ':' + checkBox[1].minute.toString()}
+        </span>
+      </div>
       <input
         className="hindingUpload"
         type="file"
