76aa6474090e2ca1cc4d7e1fcf960245679cce95
ohdair <ohdiar@gmail.com>
2022-01-24 21:50:05 +0900

    부모/자식 데이터 받기

--- front/src/components/profile/likedRoutine.tsx
+++ front/src/components/profile/likedRoutine.tsx
@@ -1,65 +1,9 @@
 import React from 'react';
 import {Header, Card, Feed, Label, Icon} from 'semantic-ui-react';
-import '../../CSS/profile/likedRoutine.css';
 import PageMover from '../../utils/PageMover';
 
-const likedRoutine: any[] = [
-  {
-    routineId: '0',
-    imgSrc:
-      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
-    title: '[1004기]RunAndLead',
-    text: '매일매일 달리기 얼마나 달릴지는 당신이 정해봐요 어디까지 길이를 표현할 수 있는지 이제 말을 해볼게요 한번 들어보실래요?',
-    timeText: '1.20 ~ 1.25',
-    location: 'gaepo',
-    participant: '(3/33)',
-    remain: 0,
-  },
-  {
-    routineId: '1',
-    imgSrc:
-      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
-    title: '그림그리기',
-    text: '같이 그림그릴래요?\n 무슨 그림그릴래요',
-    timeText: '1.22 ~ 1.31',
-    location: '온라인',
-    participant: '(5/5)',
-    remain: 2,
-  },
-  {
-    routineId: '1',
-    imgSrc:
-      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
-    title: '그림그리기',
-    text: '같이 그림그릴래요?',
-    timeText: '1.22 ~ 1.31',
-    location: '온라인',
-    participant: '(5/5)',
-    remain: 3,
-  },
-  {
-    routineId: '1',
-    imgSrc:
-      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
-    title: '그림그리기',
-    text: '같이 그림그릴래요?',
-    timeText: '1.22 ~ 1.31',
-    location: '온라인',
-    participant: '(5/5)',
-    remain: 10,
-  },
-  {
-    routineId: '1',
-    imgSrc:
-      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
-    title: '그림그리기',
-    text: '같이 그림그릴래요?',
-    timeText: '1.22 ~ 1.31',
-    location: '온라인',
-    participant: '(5/5)',
-    remain: 0,
-  },
-];
+import '../../CSS/profile/likedRoutine.css';
+
 const test: any[] = [
   {img: 'https://react.semantic-ui.com/images/avatar/small/matt.jpg'},
   {img: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg'},
@@ -74,8 +18,23 @@ const test: any[] = [
   {img: 'https://react.semantic-ui.com/images/avatar/small/helen.jpg'},
   {img: 'https://react.semantic-ui.com/images/avatar/small/joe.jpg'},
 ];
-export default function LikedRoutine(): JSX.Element {
-  console.log('hello, LikedRoutine');
+
+interface RoutineProps {
+  RoutineProp?: {
+    routineId: string;
+    imgSrc: string;
+    title: string;
+    text: string;
+    timeText: string;
+    location: string;
+    participant: string;
+    remain: number;
+  };
+}
+
+export default function LikedRoutine(
+  RoutineProps: Array<RoutineProps['RoutineProp']>,
+): JSX.Element {
   return (
     <>
       <Header as="h2" inverted className="Header">
@@ -104,13 +63,14 @@ export default function LikedRoutine(): JSX.Element {
           {/* {likedRoutine.map(e => (
           <RoutineCard key={e.routineId} {...e} />
         ))} */}
-          {likedRoutine.map(e => (
-            <Card key={e.routineId} style={{width: '90vw'}}>
+          {Object.values(RoutineProps).map((entrie, index) => (
+            <Card key={index} style={{width: '90vw'}}>
               <Card.Content>
-                {/* <Image floated="right" size="mini" src={e.imgSrc} /> */}
-                <Card.Header className="text">{e.title}</Card.Header>
-                <Card.Meta>{e.timeText}</Card.Meta>
-                <Card.Description className="text">{e.text}</Card.Description>
+                <Card.Header className="text">{entrie?.title}</Card.Header>
+                <Card.Meta>{entrie?.timeText}</Card.Meta>
+                <Card.Description className="text">
+                  {entrie?.text}
+                </Card.Description>
                 <Feed>
                   <Feed.Event
                     style={{
@@ -120,7 +80,8 @@ export default function LikedRoutine(): JSX.Element {
                     }}
                   >
                     {test.map((e, idk) => {
-                      if (idk < 5) return <Feed.Label key={e} image={e.img} />;
+                      if (idk < 5)
+                        return <Feed.Label key={idk} image={e.img} />;
                     })}
                     {test.length > 5 ? (
                       <Icon name="ellipsis horizontal" color="grey" />
@@ -129,14 +90,14 @@ export default function LikedRoutine(): JSX.Element {
                 </Feed>
               </Card.Content>
               <Card.Content extra>
-                <div className="text">{e.location}</div>
+                <div className="text">{entrie?.location}</div>
               </Card.Content>
-              {e.remain === 0 ? (
+              {entrie?.remain === 0 ? (
                 <Label attached="top right" color="red">
                   Full
                 </Label>
               ) : (
-                <Label attached="top right">{e.remain}자리 남음</Label>
+                <Label attached="top right">{entrie?.remain}자리 남음</Label>
               )}
             </Card>
           ))}
--- front/src/pages/userProfile/userProfile.tsx
+++ front/src/pages/userProfile/userProfile.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
 import LikedRoutine from '../../components/profile/likedRoutine';
+import Navbarc from '../../components/common/Navbar';
 
+const likedRoutine = [
+  {
+    routineId: '0',
+    imgSrc:
+      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
+    title: '[1004기]RunAndLead',
+    text: '매일매일 달리기 얼마나 달릴지는 당신이 정해봐요 어디까지 길이를 표현할 수 있는지 이제 말을 해볼게요 한번 들어보실래요?',
+    timeText: '1.20 ~ 1.25',
+    location: 'gaepo',
+    participant: '(3/33)',
+    remain: 0,
+  },
+  {
+    routineId: '1',
+    imgSrc:
+      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
+    title: '그림그리기',
+    text: '같이 그림그릴래요?\n 무슨 그림그릴래요',
+    timeText: '1.22 ~ 1.31',
+    location: '온라인',
+    participant: '(5/5)',
+    remain: 2,
+  },
+  {
+    routineId: '2',
+    imgSrc:
+      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
+    title: '그림그리기',
+    text: '같이 그림그릴래요?',
+    timeText: '1.22 ~ 1.31',
+    location: '온라인',
+    participant: '(5/5)',
+    remain: 3,
+  },
+  {
+    routineId: '3',
+    imgSrc:
+      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
+    title: '그림그리기',
+    text: '같이 그림그릴래요?',
+    timeText: '1.22 ~ 1.31',
+    location: '온라인',
+    participant: '(5/5)',
+    remain: 10,
+  },
+  {
+    routineId: '4',
+    imgSrc:
+      'https://cdn.crowdpic.net/list-thumb/thumb_l_9FA2E3C7E87309B6B344204FCDBFF3CE.jpg',
+    title: '그림그리기',
+    text: '같이 그림그릴래요?',
+    timeText: '1.22 ~ 1.31',
+    location: '온라인',
+    participant: '(5/5)',
+    remain: 0,
+  },
+];
 export default function UserProfile(): JSX.Element {
   return (
     <>
-      <LikedRoutine />
+      <Navbarc />
+      <LikedRoutine {...likedRoutine} />
     </>
   );
 }
